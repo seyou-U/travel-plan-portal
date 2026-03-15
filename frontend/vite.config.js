20a4ccb99e3e266d6b5ef53546f6eda3ad32b310
seyou-U <rikakorink54t@yahoo.co.jp>
2026-03-15 22:44:37 +0900

    [fix] frontendを通常フォルダとして再登録

---
+++ frontend/vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+const proxyTarget = process.env.VITE_PROXY_TARGET ?? 'http://localhost:8080'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {target: proxyTarget, changeOrigin: true}
+    },
+  },
+})
