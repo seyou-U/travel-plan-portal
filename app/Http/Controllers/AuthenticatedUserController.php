88f52fd08cc80b463a5263f7f62d7b3adb75fbb1
seyou-U <rikakorink54t@yahoo.co.jp>
2026-03-22 20:28:12 +0900

    [add] ユーザー情報取得APIの実装•追加

--- app/Http/Controllers/AuthenticatedUserController.php
+++ app/Http/Controllers/AuthenticatedUserController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class AuthenticatedUserController extends Controller
 {
-    //
+    public function show(Request $request): JsonResponse
+    {
+        $user = $request->user();
+        $iconImageUrl = null;
+
+        if ($user->icon_image_path !== null) {
+            $iconImageUrl = filter_var($user->icon_image_path, FILTER_VALIDATE_URL)
+                ? $user->icon_image_path
+                : rtrim((string) config('app.frontend_url'), '/').'/'.ltrim($user->icon_image_path, '/');
+        }
+
+        return response()->json([
+            'uuid' => $user->uuid,
+            'name' => $user->name,
+            'email' => $user->email,
+            'icon_image_url' => $iconImageUrl,
+        ]);
+    }
 }
