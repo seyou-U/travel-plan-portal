cbea5d711305a55ba8070f47fd4e9bc35252bff0
seyou-U <rikakorink54t@yahoo.co.jp>
2026-03-23 18:25:23 +0900

    [add] ログアウトAPIの実装・追加

--- routes/api.php
+++ routes/api.php
@@ -6,12 +6,11 @@ use App\Http\Controllers\LogoutController;
 use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/health', fn () => response()->json(['ok' => true]));
 Route::post('/register', [RegisterController::class, 'store']);
 Route::post('/login', [LoginController::class, 'store']);
 
 // 認証が必要なルート
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/me', [AuthenticatedUserController::class, 'show']);
-    Route::post('/logout', [LogoutController::class, 'store']);
+    Route::post('/logout', [LogoutController::class, 'destroy']);
 });
