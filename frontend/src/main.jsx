50e41c2fd592fadb7168d3ad4c800b65a5f131b7
seyou-U <rikakorink54t@yahoo.co.jp>
2026-03-24 09:52:43 +0900

    [update] フォーマッタによる修正

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,9 +4,8 @@ import { RouterProvider } from 'react-router-dom';
 import { StrictMode } from 'react';
 import './index.css';
 
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>
+  </StrictMode>,
 );
