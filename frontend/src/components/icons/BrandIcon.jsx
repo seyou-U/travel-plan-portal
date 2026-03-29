345dd02df1cae030f2b37b5ba47ad841f38d5493
seyou-U <rikakorink54t@yahoo.co.jp>
2026-03-29 13:27:12 +0900

    [add, update] それぞれのアイコンをコンポーネントとして追加、ブランドアイコンの修正

---
+++ frontend/src/components/icons/BrandIcon.jsx
@@ -0,0 +1,9 @@
+export function BrandIcon() {
+  return (
+    <div className="mx-auto flex h-9 w-9 items-center justify-center rounded-lg bg-teal-600 shadow-md shadow-teal-700/20">
+      <svg viewBox="0 0 24 24" aria-hidden="true" className="h-5 w-5 fill-white">
+        <path d="M21 16v-2l-8-5V3.5a1.5 1.5 0 1 0-3 0V9l-8 5v2l8-2.5V19l-2 1.5V22l3.5-1 3.5 1v-1.5L13 19v-5.5L21 16Z" />
+      </svg>
+    </div>
+  );
+}
---
+++ frontend/src/components/icons/LockIcon.jsx
@@ -0,0 +1,7 @@
+export function LockIcon() {
+  return (
+    <svg viewBox="0 0 24 24" aria-hidden="true" className="h-4 w-4 fill-slate-400">
+      <path d="M12 1.5A4.5 4.5 0 0 0 7.5 6v2.25h-.75A2.25 2.25 0 0 0 4.5 10.5v9A2.25 2.25 0 0 0 6.75 21.75h10.5a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25h-.75V6A4.5 4.5 0 0 0 12 1.5Zm-3 6.75V6a3 3 0 1 1 6 0v2.25H9Z" />
+    </svg>
+  );
+}
