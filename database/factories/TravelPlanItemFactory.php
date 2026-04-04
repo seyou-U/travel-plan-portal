464c0941ca83c08d43001e7c5524a8fce87f3798
seyou-U <rikakorink54t@yahoo.co.jp>
2026-04-04 22:33:49 +0900

    [add] 旅程関連API動作確認用のFactoryファイルを追加

---
+++ database/factories/TravelPlanItemFactory.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\TravelPlanDay;
+use App\Models\TravelPlanItem;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<TravelPlanItem>
+ */
+class TravelPlanItemFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $startHour = fake()->numberBetween(8, 20);
+        $startMinute = fake()->randomElement([0, 15, 30, 45]);
+        $stayMinutes = fake()->randomElement([30, 60, 90, 120]);
+
+        return [
+            'travel_plan_day_id' => TravelPlanDay::factory(),
+            'title' => fake()->randomElement([
+                '大阪城を見にいく',
+                'カフェで休憩',
+                '海遊館に行く',
+                'ホテルにチェックイン'
+            ]),
+            'spot_name' => fake()->randomElement([
+                '大阪城',
+                '海遊館',
+                '道頓堀',
+                'ユニバーサル・スタジオ・ジャパン',
+            ]),
+            'start_time' => sprintf('%02d:%s:00', $startHour, $startMinute),
+            'stay_minutes' => $stayMinutes,
+            'transportation_type' => fake()->randomElement([1, 2, 3, 4]),
+            'travel_minutes' => fake()->randomElement([10, 20, 30, 45, 60]),
+        ];
+    }
+}
