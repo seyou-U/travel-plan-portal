3dd23eaa4c0d1cdbbefa58634e570fc06df67c06
seyou-U <rikakorink54t@yahoo.co.jp>
2026-08-20 16:36:27 +0900

    [add] テスト用のダミーファイルを追加

---
+++ tests/Feature/DummyTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class DummyTest extends TestCase
+{
+    public function test_dummy(): void
+    {
+        $this->assertTrue(true);
+    }
+}
